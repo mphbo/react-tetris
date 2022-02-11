3aa253c793b6b8f65ccd4462a76f0ebeb30f07d8
Logan Thomas <logan.thomas@CO-VAN-MAC103.firmglobal.com>
2022-02-10 19:29:11 -0800

    completed most of the design and ui

---
+++ src/components/Stage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyledStage } from "./styles/StyledStage";
+import Cell from "./Cell";
+
+const Stage = ({ stage }) => {
+  const stageMapped = stage.map((row) =>
+    row.map((cell, i) => <Cell key={i} type={[0]} />)
+  );
+  return (
+    <StyledStage width={stage[0].length} height={stage.length}>
+      {stageMapped}
+    </StyledStage>
+  );
+};
+
+export default Stage;
